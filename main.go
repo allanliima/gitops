06523b7619f3b90c5f3f6e2bb927ed990d108b08
allanliima <falm.1987@gmail.com>
2024-03-25 17:20:50 -0300

    teste11

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!!"))
+		w.Write([]byte("Hello ArgoCD!!"))
 	})
 
 	http.ListenAndServe(":8081", nil)
